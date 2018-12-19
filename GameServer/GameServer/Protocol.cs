bb0df8407693769a823837c67063dab391348b68
qcyzj <qiucaoyong@163.com>
2018-12-19 17:00:06 +0800

    添加game server 协议定义文件

---
+++ GameServer/GameServer/Protocol.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GameServer.GameServer
+{
+    public static class Protocol
+    {
+        public const int GS_GW_PROTOCOL_BASE    = 1000;
+        public const int GS_GW_AUTH             = GS_GW_PROTOCOL_BASE + 1;
+    }
+}
