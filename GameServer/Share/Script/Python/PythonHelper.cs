c72f0842de34e868a9f16e4fd44a42ec23d3b602
qcyzj <qiucaoyong@163.com>
2019-02-12 09:19:56 +0800

    预备增加对python脚本的支持。

--- GameServer/Share/Script/Python/PythonHelper.cs
+++ GameServer/Share/Script/Python/PythonHelper.cs
@@ -1,8 +1,47 @@
 ﻿
+using Microsoft.Scripting.Hosting;
+
 namespace Share.Script.Python
 {
     public class PythonHelper
     {
-        
+        private ScriptEngine m_Engine;
+        private ScriptScope m_Scope;
+
+
+        public PythonHelper()
+        {
+            m_Engine = IronPython.Hosting.Python.CreateEngine();
+            m_Scope = m_Engine.CreateScope();
+        }
+
+
+        public dynamic LoadScriptFromFile(string path)
+        {
+            ScriptSource source = m_Engine.CreateScriptSourceFromFile(path);
+            return source.Execute(m_Scope);
+        }
+
+        public dynamic LoadScriptFromString(string expr)
+        {
+            ScriptSource source = m_Engine.CreateScriptSourceFromString(expr);
+            return source.Execute(m_Scope);
+        }
+
+
+        public dynamic GetVariable(string name)
+        {
+            return m_Scope.GetVariable(name);
+        }
+
+        public T GetFunction<T>(string name)
+        {
+            return m_Scope.GetVariable<T>(name);
+        }
+
+        public T GetClass<T>(string name)
+        {
+            return m_Scope.GetVariable<T>(name);
+        }
     }
 }
