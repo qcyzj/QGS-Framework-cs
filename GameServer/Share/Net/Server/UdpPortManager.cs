76146cd0b187330fc51973b08258f451e40208f7
qiucaoyong <qiucaoyong@163.com>
2019-05-17 14:56:24 +0800

    修改部分代码中的集合为并发集合。

--- GameServer/Share/Net/Server/UdpPortManager.cs
+++ GameServer/Share/Net/Server/UdpPortManager.cs
@@ -1,5 +1,5 @@
 ﻿using System.Diagnostics;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace Share.Net.Server
 {
@@ -10,14 +10,12 @@ namespace Share.Net.Server
         public const int INVALID_UDP_PORT = 0;
 
 
-        private Stack<int> m_PortStack;
-        private object m_PortLock;
+        private ConcurrentStack<int> m_PortStack;
 
 
         public UdpPortManager()
         {
-            m_PortStack = new Stack<int>();
-            m_PortLock = new object();
+            m_PortStack = new ConcurrentStack<int>();
         }
 
 
@@ -42,27 +40,20 @@ namespace Share.Net.Server
 
         public int AllocatePort()
         {
-            int port = INVALID_UDP_PORT;
-
-            lock (m_PortLock)
+            if (m_PortStack.TryPop(out int port))
+            {}
+            else
             {
-                if (m_PortStack.Count > 0)
-                {
-                    port = m_PortStack.Pop();
-                }
+                port = INVALID_UDP_PORT;
             }
 
             return port;
         }
 
-        public void FreePort(int port)
+        public void DeallocatePort(int port)
         {
             Debug.Assert(INVALID_UDP_PORT != port);
-
-            lock (m_PortLock)
-            {
-                m_PortStack.Push(port);
-            }
+            m_PortStack.Push(port);
         }
     }
 }
