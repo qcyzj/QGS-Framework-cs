3bc0555f46d845aabd3a2981a00faf5a6ec2ae31
qiucaoyong <qiucaoyong@163.com>
2019-06-27 14:47:46 +0800

    修改log双缓存交换失败的bug

--- AccountServer/Share/Net/Buffer/ReadWriteBuffer.cs
+++ AccountServer/Share/Net/Buffer/ReadWriteBuffer.cs
@@ -11,13 +11,8 @@ namespace Share.Net.Buffer
 
 
         private byte[] m_Buffer;
-        private int m_BufReadIndex;
-        private int m_BufWriteIndex;
-
-
-        public byte[] Buffer { get { return m_Buffer; } }
-        public int ReadIndex { get { return m_BufReadIndex; } }
-        public int WriteIndex { get { return m_BufWriteIndex; } }
+        private int m_ReadIndex;
+        private int m_WriteIndex;
 
 
         public ReadWriteBuffer(byte[] buffer)
@@ -36,11 +31,16 @@ namespace Share.Net.Buffer
 
         public void SetEmpty()
         {
-            m_BufReadIndex = m_BufWriteIndex = 0;
+            m_ReadIndex = m_WriteIndex = 0;
         }
 
         
         public void WriteBytes(byte[] buf, int buf_len)
+        {
+            WriteBytes(buf, 0, buf_len);
+        }
+
+        public void WriteBytes(byte[] buf, int off_set, int buf_len)
         {
             if (GetCanWriteSize() < buf_len)
             {
@@ -49,35 +49,35 @@ namespace Share.Net.Buffer
 
             Debug.Assert(ValidWriteSize(buf_len));
 
-            Array.Copy(buf, 0, m_Buffer, m_BufWriteIndex, buf_len);
-            m_BufWriteIndex += buf_len;
+            Array.Copy(buf, off_set, m_Buffer, m_WriteIndex, buf_len);
+            m_WriteIndex += buf_len;
         }
 
         public void ReadBytes(byte[] buf, int buf_len)
         {
             Debug.Assert(ValidReadSize(buf_len));
 
-            Array.Copy(m_Buffer, m_BufReadIndex, buf, 0, buf_len);
-            m_BufReadIndex += buf_len;
+            Array.Copy(m_Buffer, m_ReadIndex, buf, 0, buf_len);
+            m_ReadIndex += buf_len;
 
-            if (m_BufReadIndex == m_BufWriteIndex)
+            if (m_ReadIndex == m_WriteIndex)
             {
                 SetEmpty();
             }
         }
-
+        
         [Obsolete("This function is obsoleted, using PeekPacketSize() instead.", true)]
         public void PeekPacketHead(byte[] buf)
         {
             Debug.Assert(buf.Length >= Packet.PACKET_HEAD_LENGTH);
             Debug.Assert(ValidReadSize(Packet.PACKET_HEAD_LENGTH));
 
-            Array.Copy(m_Buffer, m_BufReadIndex, buf, 0, Packet.PACKET_HEAD_LENGTH);
+            Array.Copy(m_Buffer, m_ReadIndex, buf, 0, Packet.PACKET_HEAD_LENGTH);
         }
 
         public int PeekPacketSize()
         {
-            ReadOnlySpan<byte> buf_arr = new ReadOnlySpan<byte>(m_Buffer, m_BufReadIndex,
+            ReadOnlySpan<byte> buf_arr = new ReadOnlySpan<byte>(m_Buffer, m_ReadIndex, 
                                                                 Packet.PACKET_SIZE_LENGTH);
             return BitConverter.ToInt16(buf_arr);
         }
@@ -88,55 +88,45 @@ namespace Share.Net.Buffer
 
             if (read_size > 0)
             {
-                Array.Copy(m_Buffer, m_BufReadIndex, m_Buffer, 0, read_size);
+                Array.Copy(m_Buffer, m_ReadIndex, m_Buffer, 0, read_size);
             }
 
-            m_BufWriteIndex -= m_BufReadIndex;
-            m_BufReadIndex = 0;
+            m_WriteIndex -= m_ReadIndex;
+            m_ReadIndex = 0;
         }
 
 
         public int GetCanReadSize()
         {
             ValidIndex();
-            return m_BufWriteIndex - m_BufReadIndex;
+            return m_WriteIndex - m_ReadIndex;
         }
 
         public int GetCanWriteSize()
         {
-            Debug.Assert(m_BufWriteIndex <= BUFFER_MAX_SIZE);
-            return BUFFER_MAX_SIZE - m_BufWriteIndex;
-        }
-
-        public void AddReadSize(int size)
-        {
-            Debug.Assert(ValidReadSize(size));
-            m_BufReadIndex += size;
-        }
-
-        public void AddWriteSize(int size)
-        {
-            Debug.Assert(ValidWriteSize(size));
-            m_BufWriteIndex += size;
+            Debug.Assert(m_WriteIndex <= BUFFER_MAX_SIZE);
+            return BUFFER_MAX_SIZE - m_WriteIndex;
         }
 
 
         private bool ValidWriteSize(int size)
         {
             ValidIndex();
-            return m_BufWriteIndex + size < BUFFER_MAX_SIZE;
+            return m_WriteIndex + size <= BUFFER_MAX_SIZE;
         }
 
         private bool ValidReadSize(int size)
         {
             ValidIndex();
-            return m_BufReadIndex + size <= m_BufWriteIndex;
+            return m_ReadIndex + size <= m_WriteIndex;
         }
 
         private void ValidIndex()
         {
-            Debug.Assert(m_BufReadIndex >= 0);
-            Debug.Assert(m_BufWriteIndex >= m_BufReadIndex);
+            Debug.Assert(m_ReadIndex >= 0);
+            Debug.Assert(m_WriteIndex >= m_ReadIndex);
+            Debug.Assert(m_ReadIndex < BUFFER_MAX_SIZE);
+            Debug.Assert(m_WriteIndex < BUFFER_MAX_SIZE);
         }
     }
 }
